fadb2faa82391cf80a7fa238719efb0b6634edc9
Frost <mpyang2021@gmail.com>
2021-10-07 22:07:11 -0500

    fixed id tags and localstorage

--- script.js
+++ script.js
@@ -21,15 +21,42 @@ $(document).ready(function () {
             console.log(hour)
 
             if(hour === currentHour) {
-                $(this).children(".col-md-10").attr("class", "present col-sm-10 description")
+                $(this).children(".col-md-10").attr("class", "present col-md-10 description")
             } else if (currentHour > hour) {
-                $(this).children(".col-md-10").attr("class", "past col-sm-10 description")
+                $(this).children(".col-md-10").attr("class", "past col-md-10 description")
             } else {
-                $(this).children(".col-md-10").attr("class", "future col-sm-10 description")
+                $(this).children(".col-md-10").attr("class", "future col-md-10 description")
             }
         })
     }
 
 timeNow() 
+
+// event save - local storage
+
+saveButton.on("click", function(event) {
+    event.preventDefault();
+    var data = $(this).siblings(".col-md-10").val().replace(/['"]+/g, '');
+    var topData = $(this).parent().attr("id");
+    localStorage.setItem(topData, JSON.stringify(data));
+
+})
+// recall data
+
+
+$("#9 textarea").val(localStorage.getItem('9').replace(/['"]+/g, ''));
+$("#10 textarea").val(localStorage.getItem('10').replace(/['"]+/g, ''));
+$("#11 textarea").val(localStorage.getItem('11').replace(/['"]+/g, ''));
+$("#12 textarea").val(localStorage.getItem('12').replace(/['"]+/g, ''));
+$("#13 textarea").val(localStorage.getItem('13').replace(/['"]+/g, ''));
+$("#14 textarea").val(localStorage.getItem('14').replace(/['"]+/g, ''));
+$("#15 textarea").val(localStorage.getItem('15').replace(/['"]+/g, ''));
+$("#16 textarea").val(localStorage.getItem('16').replace(/['"]+/g, ''));
+$("#17 textarea").val(localStorage.getItem('17').replace(/['"]+/g, ''));
+
+
+
+
 });
+
 
